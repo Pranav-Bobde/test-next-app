024beec5a352699a5239867625d144d431184b42
Pranav Bobde <bobdep31@gmail.com>
2023-09-06 01:02:51 +0530

    copy paste

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,29 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export default async function middleware(request: NextRequest) {
-    console.log("hostname", request.nextUrl.host);
+  console.log("hostname", request.nextUrl.host);
 
-    const host = request.nextUrl.host;
+  const host = request.nextUrl.host;
 
-    if (host === "pranavbobde.com") {
-        return NextResponse.redirect("https://aniwave.to/");
-    }
+  if (host === "pranavbobde.com")
+    return NextResponse.redirect("https://beta.nextjs.org");
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
+
+// Stop Middleware running on static files and public folder
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     * - public folder
+     */
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
+};
